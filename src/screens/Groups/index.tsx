8ea50d3a6721d5687db3c98e8d6f258ada9fb9dc
mateus <mateusweslley14@gmail.com>
2024-12-13 11:19:17 -0300

    create component list empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -7,9 +7,10 @@ import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 
 import { Container } from "./styles";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -20,6 +21,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   );
