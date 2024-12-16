10bfd212bcc80f6be0aea07bef3f9801ffc446a9
mateus <mateusweslley14@gmail.com>
2024-12-16 11:19:46 -0300

    new: Criando o componente ButtonIcon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,8 @@
-import { Header } from "@components/Header";
 import { Container } from "./styles";
+
+import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
 
 export function Players() {
   return (
@@ -11,6 +13,8 @@ export function Players() {
         title="Nome da turma"
         subtitle="Adicione a galera e separe os times"
       />
+
+      <ButtonIcon />
     </Container>
   );
 }
