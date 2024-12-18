13af66981ab8f1ad6bf458cf3d69cfea2975f852
mateus <mateusweslley14@gmail.com>
2024-12-18 09:47:59 -0300

    Criando o componente playercard

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -8,10 +8,11 @@ import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
+import { PlayerCard } from "@components/PlayerCard";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
-  const [players, setPlayers] = useState(["Mateus", "Marcos", "Lucas", "João"]);
+  const [players, setPlayers] = useState(["Mateus", "Marcos"]);
 
   return (
     <Container>
@@ -42,6 +43,14 @@ export function Players() {
         />
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+      />
     </Container>
   );
 }
