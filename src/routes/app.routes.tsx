a65fe07ce1c710f8ba75686723484f268c49088e
mateus <mateusweslley14@gmail.com>
2024-12-18 11:24:18 -0300

    estrutura de navegação

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -8,7 +8,11 @@ const { Screen, Navigator } = createNativeStackNavigator();
 
 export function AppRoutes() {
   return (
-    <Navigator>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Screen name="groups" component={Groups} />
       <Screen name="new" component={NewGroup} />
       <Screen name="players" component={Players} />
